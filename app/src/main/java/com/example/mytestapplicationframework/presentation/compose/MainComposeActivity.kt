67af3c8c1e200bfc58138f2419c99015d7c0e5c6
Dante <zcufo123@gmail.com>
2022-11-20 21:09:52 +1300

    Add some compose example and change some configuration.

--- app/src/main/java/com/example/mytestapplicationframework/presentation/compose/MainComposeActivity.kt
+++ app/src/main/java/com/example/mytestapplicationframework/presentation/compose/MainComposeActivity.kt
@@ -3,36 +3,53 @@ package com.example.mytestapplicationframework.presentation.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mytestapplicationframework.presentation.compose.ui.theme.MyCleanFrameworkTheme
 
 class MainComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyCleanFrameworkTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("MyCleanFramework")
-                }
-            }
+            DefaultPreview()
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Column {
+            Text(text = "Hello $name!")
+            Text(text = "Hi $name!")
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = "Hello1 $name!")
+            Text(text = "Hi1 $name!")
+        }
+    }
+}
+
+@Composable
+fun List(texts: Array<String>) {
+    LazyColumn {
+        items(texts) { message ->
+            Text(text = "Hi $message")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyCleanFrameworkTheme {
-        Greeting("MyCleanFramework")
+        List(arrayOf("a","b","c","d","e"))
     }
 }
